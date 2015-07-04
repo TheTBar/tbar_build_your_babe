2da80d67ec1d6f62f797845161419218e080c8ea
adam <adam@splickit.com>
2015-07-04 12:04:02 -0600

    refactor

--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -12,7 +12,7 @@ module Spree
 
     def self.get_babes_available_package_list(babe)
 
-    # perhaps could use this to get all products and then see if the current product exists.  woudl be far less queries
+    # perhaps could use this to get all products and then see if the current product exists in that size.  would be far less queries
       # if performance is bad the other way may have to invoke this.
     # Spree::Product.joins(:variants_including_master => :option_values)
     #   .where('spree_option_values.presentation = ? AND
@@ -20,17 +20,12 @@ module Spree
     #          Spree::OptionType.find_by_name('tshirt-size').id)
 
       available_taxons = []
-      taxons = get_babes_package_list(babe)
-      taxons.each do |taxon|
+      get_babes_package_list(babe).each do |taxon|
         taxon_is_available = true
         taxon.products.each do |product|
-          if !product.does_product_have_stock_on_hand_for_option_value?(babe.size_value_for_size_option_type_name(product.product_size_type))
-            taxon_is_available = false
-          end
+          taxon_is_available = false unless product.does_product_have_stock_on_hand_for_option_value?(babe.size_value_for_size_option_type_name(product.product_size_type))
         end
-        if taxon_is_available
-          available_taxons.push(taxon)
-        end
+        available_taxons.push(taxon) if taxon_is_available
       end
       available_taxons
     end

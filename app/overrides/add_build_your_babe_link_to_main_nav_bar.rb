c00c6fbf916f4b0608879d33f9aed991fb034332
adam <adam@splickit.com>
2015-07-12 14:14:06 -0600

    removing link. will now reference in main application

--- app/overrides/add_build_your_babe_link_to_main_nav_bar.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/shared/_main_nav_bar',
-                     :name => 'add_build_your_babe_link_to_main_nav',
-                     :insert_after => "li#home-link",
-                     :text => "<li id=\"build-your-babe-link\" data-hook><%= link_to Spree.t('build your babe'), build_your_babe_path %></li>")
